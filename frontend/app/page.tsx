643d8453e4971d8ce2c7bd8c7c3908b5749c69e5
graviolagames <graviolagames@gmail.com>
2023-09-26 07:32:23 -0300

    [homolog] Modified page to show homologation test message

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
       <div>
-      <h1> Bem vindo ao app Conchayoro </h1>
+        <h1> Bem vindo ao app Conchayoro </h1>
+        <h2> Teste de Homologação </h2>
       </div>
     )
   }
